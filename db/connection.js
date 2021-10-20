a225e1136e06598a66f642e80388fcf29278388a
Hardik Rola <hrsautomation20@gmail.com>
2021-10-20 14:39:57 -0400

    SQL Connection created

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,15 @@
 //MySQL Connection
+const mysql = require("mysql2");
+
+//dotenv - https://www.npmjs.com/package/dotenv
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3001,
+  user: "root",
+  password: "987654321",
+  database: "employees",
+});
+
+module.exports = connection;
